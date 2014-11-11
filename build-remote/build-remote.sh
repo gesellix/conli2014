431d7dd954fe4d5d7da89c2871c59df5b2cf80c9
Tobias Gesellchen <tobias@gesellix.de>
2014-11-11 09:41:43 +0100

    make it work

--- build-remote/build-remote.sh
+++ build-remote/build-remote.sh
@@ -8,4 +8,5 @@ curl -X DELETE "$DOCKER_HOST/containers/build-result"
 echo "create build-result..."
 curl -X POST "$DOCKER_HOST/containers/create?name=build-result" -d '{"Image":"projekt-builder"}' -H "Content-Type: application/json"
 echo "extract build-result..."
+mkdir -p ./target
 curl -X POST "$DOCKER_HOST/containers/build-result/copy" -d '{"Resource":"/opt/build-result.txt"}' -H "Content-Type: application/json" | tar xm -C ./target/
